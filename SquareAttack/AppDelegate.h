af246c7a12b5c2d3715897cdd74b33451d52a198
Anton Orzes <antonorzes@Antons-Mac-mini.local>
2017-04-16 20:16:38 +0200

    Initial Commit

---
+++ SquareAttack/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SquareAttack
+//
+//  Created by Anton Orzes on 16/04/2017.
+//  Copyright © 2017 Anton Orzes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SquareAttack/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  SquareAttack
+//
+//  Created by Anton Orzes on 16/04/2017.
+//  Copyright © 2017 Anton Orzes. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
